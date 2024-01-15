b874c213d044dfe674066bd6b0329d4d3c528f1a
Carol Wargo <Carolwargo@gmail.com>
2024-01-15 06:43:58 -0500

    update

--- src/components/Graphics/Graphics.jsx
+++ src/components/Graphics/Graphics.jsx
@@ -15,6 +15,10 @@ import C3 from '../../assets/images/Cutters/C3.png';
 import C4 from '../../assets/images/Cutters/C4.png';
 //import C5 from '../../assets/images/Cutters/C5.png';
 
+import Canceled from '../../assets/images/Graphics/Canceled.png';
+import CuttersFlyers from '../../assets/images/Graphics/CuttersFlyers.png';
+import NorthernGolf from '../../assets/images/Graphics/NorthernGolf.png';
+
 
 
 const BusinessEssentials = () => {
@@ -477,6 +481,221 @@ const BusinessEssentials = () => {
     </div>
 
     </div>
+
+
+    {/* fifth row */  }
+    <div className="row graphics-container">
+    <div className="col-sm-12 col-md-3 col-lg-3 d-flex justify-content-center align-bottom">
+      <div className="graphics-body p-4 justify-content-center align-bottom">
+        <div className="graphics-title">
+          <img
+            src={NorthernGolf}
+            alt="NorthernGolf"
+            className="mb-2 border border-black clickable-image"
+            style={{ width: "80%", height: "auto" }}
+            onClick={handleModalOpen}
+          />
+          <p>SHOP SINGLE ITEMS OR PACKAGE OPTIONS</p>
+          <h1>BUSINESS ESSENTIALS</h1>
+        </div>
+        <p className="business-essentials-text">
+          Explore our range of "Business Essentials" custom products tailored to your requirements. We offer a variety of design solutions to include graphic and digital design, promotional campaigns, as well as software solutions and server-side integrations.
+        </p>
+      </div>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={NorthernGolf}
+            alt="NorthernGolf"
+            className="w-100 h-auto"
+          />
+        </Modal.Body>
+       
+      </Modal>
+    </div>
+    <div className="col-sm-12 col-md-3 col-lg-3 d-flex justify-content-center align-bottom">
+      <div className="graphics-body p-4 justify-content-center align-bottom">
+        <div className="graphics-title">
+          <img
+            src={C3}
+            alt="C3"
+            className="mb-2 border border-black clickable-image"
+            style={{ width: "80%", height: "auto" }}
+            onClick={handleModalOpen}
+          />
+          <p>SHOP SINGLE ITEMS OR PACKAGE OPTIONS</p>
+          <h1>BUSINESS ESSENTIALS</h1>
+        </div>
+        <p className="graphics-text">
+          Explore our range of "Business Essentials" custom products tailored to your requirements. We offer a variety of design solutions to include graphic and digital design, promotional campaigns, as well as software solutions and server-side integrations.
+        </p>
+      </div>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{fontSize:'1rem'}}>click to close</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={C3}
+            alt="C3"
+            className="w-100 h-auto"
+          />
+        </Modal.Body>
+     
+      </Modal>
+    </div>
+    </div>
+
+
+
+
+
+    <div className="row graphics-container">
+    <div className="col-sm-12 col-md-6 col-lg-6 d-flex justify-content-center align-bottom">
+      <div className="graphics-body p-4 justify-content-center align-bottom">
+        <div className="graphics-title">
+          <img
+            src={CuttersFlyers}
+            alt="CuttersFlyers"
+            className="mb-2 border border-black clickable-image"
+            style={{ width: "80%", height: "auto" }}
+            onClick={handleModalOpen}
+          />
+          <p>SHOP SINGLE ITEMS OR PACKAGE OPTIONS</p>
+          <h1>BUSINESS ESSENTIALS</h1>
+        </div>
+        <p className="business-essentials-text">
+          Explore our range of "Business Essentials" custom products tailored to your requirements. We offer a variety of design solutions to include graphic and digital design, promotional campaigns, as well as software solutions and server-side integrations.
+        </p>
+      </div>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={CuttersFlyers}
+            alt="CuttersFlyers"
+            className="w-100 h-auto"
+          />
+        </Modal.Body>
+       
+      </Modal>
+    </div>
+    <div className="col-sm-12 col-md-6 col-lg-6 d-flex justify-content-center align-bottom">
+      <div className="graphics-body p-4 justify-content-center align-bottom">
+        <div className="graphics-title">
+          <img
+            src={NorthernGolf}
+            alt="NorthernGolf"
+            className="mb-2 border border-black clickable-image"
+            style={{ width: "80%", height: "auto" }}
+            onClick={handleModalOpen}
+          />
+          <p>SHOP SINGLE ITEMS OR PACKAGE OPTIONS</p>
+          <h1>BUSINESS ESSENTIALS</h1>
+        </div>
+        <p className="graphics-text">
+          Explore our range of "Business Essentials" custom products tailored to your requirements. We offer a variety of design solutions to include graphic and digital design, promotional campaigns, as well as software solutions and server-side integrations.
+        </p>
+      </div>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{fontSize:'1rem'}}>click to close</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={NorthernGolf}
+            alt="NorthernGolf"
+            className="w-100 h-auto"
+          />
+        </Modal.Body>
+      </Modal>
+    </div>
+    </div>
+
+
+
+
+
+
+    <div className="row graphics-container">
+    <div className="col-sm-12 col-md-6 col-lg-6 d-flex justify-content-center align-bottom">
+      <div className="graphics-body p-4 justify-content-center align-bottom">
+        <div className="graphics-title">
+          <img
+            src={Canceled}
+            alt="Canceled"
+            className="mb-2 border border-black clickable-image"
+            style={{ width: "80%", height: "auto" }}
+            onClick={handleModalOpen}
+          />
+          <p>SHOP SINGLE ITEMS OR PACKAGE OPTIONS</p>
+          <h1>BUSINESS ESSENTIALS</h1>
+        </div>
+        <p className="business-essentials-text">
+          Explore our range of "Business Essentials" custom products tailored to your requirements. We offer a variety of design solutions to include graphic and digital design, promotional campaigns, as well as software solutions and server-side integrations.
+        </p>
+      </div>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={Canceled}
+            alt="Canceled"
+            className="w-100 h-auto"
+          />
+        </Modal.Body>
+       
+      </Modal>
+    </div>
+    <div className="col-sm-12 col-md-6 col-lg-6 d-flex justify-content-center align-bottom">
+      <div className="graphics-body p-4 justify-content-center align-bottom">
+        <div className="graphics-title">
+          <img
+            src={301}
+            alt="301"
+            className="mb-2 border border-black clickable-image"
+            style={{ width: "80%", height: "auto" }}
+            onClick={handleModalOpen}
+          />
+          <p>SHOP SINGLE ITEMS OR PACKAGE OPTIONS</p>
+          <h1>BUSINESS ESSENTIALS</h1>
+        </div>
+        <p className="graphics-text">
+          Explore our range of "Business Essentials" custom products tailored to your requirements. We offer a variety of design solutions to include graphic and digital design, promotional campaigns, as well as software solutions and server-side integrations.
+        </p>
+      </div>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{fontSize:'1rem'}}>click to close</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={301}
+            alt="301"
+            className="w-100 h-auto"
+          />
+        </Modal.Body>
+      </Modal>
+    </div>
+    </div>
+
+
+
+
+
+
+
+
      </div>
   );
 };
